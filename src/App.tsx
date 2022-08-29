8a3f638ac48d3591f56b3d7f21ba4ea891377e51
Daniel Scally <44758401+dascally@users.noreply.github.com>
2022-08-29 16:41:01 -0400

    Add stub for md-to-html form and submit handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { FormEvent } from 'react';
 import './App.css';
 
 function App() {
+  function handleConvertSubmit(evt: FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+
+    if (evt.currentTarget instanceof HTMLFormElement) {
+      const mdInputElt = evt.currentTarget.elements.namedItem('md');
+      const htmlOutputElt = evt.currentTarget.elements.namedItem('html');
+
+      if (!(mdInputElt instanceof HTMLTextAreaElement)) {
+        throw new Error('Input field not found.');
+      }
+      if (!(htmlOutputElt instanceof HTMLOutputElement)) {
+        throw new Error('Output field not found.');
+      }
+
+      const mdInput = mdInputElt.value;
+      htmlOutputElt.value = mdInput;
+    }
+  }
+
   return (
     <>
       <hgroup>
@@ -10,7 +30,19 @@ function App() {
           highlighting.
         </p>
       </hgroup>
-      <p>Here's a placeholder paragraph.</p>
+      <p>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        Here's a placeholder paragraph with an <a href='#'>example link</a>.
+      </p>
+      <form className='mdToHTMLForm' onSubmit={handleConvertSubmit}>
+        <textarea name='md' cols={80} rows={20}>
+          {'# Sample Markdown\n\n' +
+            '1. Enter some content here ...\n' +
+            "2. ... and it'll be converted to HTML."}
+        </textarea>
+        <button type='submit'>Convert</button>
+        <output name='html' />
+      </form>
     </>
   );
 }
